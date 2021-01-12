c3d8fdf789856e1584227dbd519725c75f6d538a
Aline Alves <alinefsalvessi@gmail.com>
2021-01-12 20:04:48 -0300

    Adicionando Paginas

---
+++ src/AppNavigator.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Home from './modules/Home'
+import MyAccount from './modules/MyAccount'
+
+const AppNavigator = () => {
+const { Navigator, Screen } = createStackNavigator()
+  return (
+   <NavigationContainer>
+       <Navigator>
+           <Screen name="Home" component={ Home } />
+           <Screen name="Profile" component={ MyAccount } />
+       </Navigator>
+   </NavigationContainer>
+  )
+}
+
+export default AppNavigator
